24f62f5bbb166c13f189b80627f8c52d5652a7be
Hochan Kim <ghckskim@gmail.com>
2026-07-08 11:54:18 +0900

    26.07.08 데이터 컴포넌트화 수정 작업 진행 중

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,36 +1,44 @@
 import { useState, useEffect, use } from 'react'
 import companyLogo from '../assets/companyLogo.png'
+import menuData from '../data/menuData'
 
 function Header() {
   // 헤더 컴포넌트
   return (
     <header>
-     <div 
-        className='
+      <div className='
          w-9/10 
          mx-auto 
          flex 
          items-center 
          justify-between py-4'
-     >
-      <a 
-        href="#none"
-        className='
+      >
+        {/* 회사 로고 */}
+        <a href="#none"
+         className='
          inline-block 
          w-1/5'
-      >
-        <img src={companyLogo} 
-            alt="Company Logo" 
-            className='w-100%'
-        />
-      </a>
-      
-      
-      {/* 헤더 오른쪽 연락처 */}
-      <ul>
-        <li className='text-2xl font-bold'>&#9742; 02-564-8888</li>
-      </ul>
-     </div>
+        >
+          <img src={companyLogo} 
+              alt="Company Logo" 
+              className='w-100%'
+          />
+        </a>
+        
+        {/* 헤더 메뉴 */}
+        <ul className='flex'>
+          {menuData.map((menu) => (
+            <li key={menu.id}>
+              {menu.title}
+            </li>
+          ))}
+        </ul>
+        
+        {/* 헤더 오른쪽 연락처 */}
+        <ul>
+          <li className='text-2xl font-bold'>&#9742; 02-564-8888</li>
+        </ul>
+      </div>
     </header>
   )
 }
--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,5 +1,6 @@
 const menuData = [
   {
+    // 헤더 1st 메인 메뉴
     id: 1,
     title: "회사소개",
     link: "/company",
@@ -10,6 +11,7 @@ const menuData = [
     ]
   },
   {
+    // 헤더 2nd 메인 메뉴
     id: 2,
     title: "투자이민 프로젝트",
     link: "/project",
@@ -17,19 +19,48 @@ const menuData = [
       "AAEB5",
       "Bether Capital",
       "EB5Capital",
-      "EB5AN"
+      "EB5AN",
     ]
   },
   {
+    // 헤더 3rd 메인 메뉴
     id: 3,
     title: "국가별 이민 안내",
     link: "/country",
     subMenus: [
       "미국",
       "캐나다",
-      "호주"
+      "유럽",
+      "아시아",
+      "오세아니아",
+      "중남미/카리브해"
     ]
-  }
+  },
+  {
+    // 헤더 4th 메인 메뉴
+    id: 4,
+    title: "성공 사례",
+    link: "/country",
+    subMenus: [
+      "투자이민 수속",
+      "전문가 이민 수속",
+      "취업이민 수속",
+      "유럽이민 수속",
+      "거주비자 수속",
+      "시민권 수속"
+    ]
+  },
+  {
+    // 헤더 5th 메인 메뉴
+    id: 5,
+    title: "고객센터",
+    link: "/country",
+    subMenus: [
+      "공지사항",
+      "제휴 문의",
+      "행사 신청",
+    ]
+  },
 ];
 
 export default menuData;
